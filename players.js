3218ff4723ce2f59272141be78b8ee1e37c2506e
gamingvyn <mervyn6911@gmail.com>
2025-10-30 20:56:04 +0800

    Implement Player class with abilities and movement

---
+++ players.js
@@ -0,0 +1,86 @@
+// Players & Abilities
+
+class Player {
+    constructor(name, imgSrc, side, ability) {
+        this.name = name;
+        this.img = new Image();
+        this.img.src = imgSrc;
+        this.x = side === 'left' ? 100 : 700;
+        this.y = 400;
+        this.vx = 0;
+        this.vy = 0;
+        this.speed = 4;
+        this.width = 50;
+        this.height = 80;
+        this.side = side;
+        this.hasBall = false;
+        this.jumpPower = 12;
+        this.onGround = true;
+        this.ability = ability; // megaDunk, shield, superAlley
+        this.abilityCooldown = 0;
+    }
+
+    move(direction) {
+        this.vx = direction * this.speed;
+    }
+
+    jump() {
+        if (this.onGround) {
+            this.vy = -this.jumpPower;
+            this.onGround = false;
+        }
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        // Gravity
+        this.vy += 0.5;
+
+        // Floor collision
+        if (this.y >= 400) {
+            this.y = 400;
+            this.vy = 0;
+            this.onGround = true;
+        }
+
+        // Cooldown
+        if (this.abilityCooldown > 0) this.abilityCooldown--;
+    }
+
+    shoot(ball, isPlayer=true) {
+        if (!ball.isFlying && this.hasBall) {
+            let distance = Math.hypot(this.x - ball.hoopX, this.y - ball.hoopY);
+            let chance = distance < 200 ? 0.8 : 0.6;
+            if (!isPlayer && Math.random() < 1) chance = 1; // AI always tries
+            if (Math.random() <= chance) {
+                ball.flyToHoop(this.x, this.y);
+            } else {
+                ball.missShot(this.x, this.y);
+            }
+            this.hasBall = false;
+        }
+    }
+
+    useAbility(ball, opponents) {
+        if (this.abilityCooldown === 0 && this.ability && this.hasBall) {
+            if (this.ability === 'megaDunk') {
+                ball.megaDunk(this.x, this.y);
+            } else if (this.ability === 'shield') {
+                opponents.forEach(op => op.shield = 100);
+            } else if (this.ability === 'superAlley') {
+                ball.superAlley(this.x, this.y);
+            }
+            this.hasBall = false;
+            this.abilityCooldown = 300; // cooldown frames
+        }
+    }
+}
+
+// Example players
+const players = [
+    new Player('Player1', 'assets/player1.png', 'left', 'megaDunk'),
+    new Player('Player2', 'assets/player2.png', 'right', 'shield'),
+    new Player('Player3', 'assets/player3.png', 'left', 'superAlley')
+];
